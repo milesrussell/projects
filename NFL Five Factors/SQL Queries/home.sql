918044e910f05f744bbcb9f17bf1bea64ad4a304
miles.russell <miles.russell@hudl.com>
2019-04-25 08:20:06 -0500

    moving files

--- NFL Five Factors/SQL Queries/home.sql
+++
@@ -1,9 +0,0 @@
-/*
-This query returns the gameid, the home team, and whether the home team won.
-*/
-SELECT gid AS game_id,
-       h AS home_team,
-       CASE WHEN ptsh > ptsv THEN 1 ELSE 0 END AS win
-FROM game
-WHERE seas BETWEEN 2010 AND 2015
- AND ptsh != ptsv;
--- NFL Five Factors/SQL Queries/visiting.sql
+++
@@ -1,9 +0,0 @@
-/*
-This query returns the gameid, the visiting team, and whether the visiting team won.
-*/
-SELECT gid AS game_id,
-       v AS visiting_team,
-       CASE WHEN ptsv > ptsh THEN 1 ELSE 0 END AS win
-FROM game
-WHERE seas BETWEEN 2010 AND 2015
- AND ptsh - ptsv != 0;
